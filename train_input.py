73f3c9c55fb6234044a674952616bece29aa60f0
echanhe <csyichenhe@gmail.com>
2018-04-01 16:52:41 +0100

    add cpm

--- train_input.py
+++ train_input.py
@@ -69,13 +69,14 @@ def set_x_img(df , scale = 1 , folder =  'train/'):
         np_img = np.array(img)
         x_all[i,:,:,:] = np_img
         i+=1
+        x_all
 #     print(x_all.shape)
 #     x_all=x_all.reshape((size,-1))
 #     print(x_all.shape)
 #     np.savetxt('images.txt' , x_all)
     return x_all
 # y (m,关键点*3)
-def set_y_coord(df,scale = 1):
+def set_y_coord(df,scale = 1,coord_only = False):
     columns = df.columns
     if "height" in columns or "width" in columns:
         l_m_columns = columns.drop(['image_id' , 'image_category','height','width'])
@@ -101,7 +102,8 @@ def set_y_coord(df,scale = 1):
     is_vis_data = y_coord[:,vis_index]
     is_vis_data[np.logical_or( is_vis_data ==-1 , is_vis_data==0 )]=0
     
-  
+    if coord_only:
+        return y_coord[:,l_m_index]
     return_array = np.concatenate((y_coord[:,l_m_index],has_lm_data , is_vis_data),axis=1)
     return return_array
     #x1,y1 ... xn, yn
@@ -118,18 +120,54 @@ def set_y_map(df,scale = 1):
     y_coord = df[l_m_columns].as_matrix()
     lm_cnt = int(y_coord.shape[1]/3)
     df_size = y_coord.shape[0]
-    size = int(512/(scale*8)-1)
+    size = int(512/(scale*8))
+    real_scale = 512/size
     y_map = np.zeros((df_size,size,size,lm_cnt))
 
     for j in range(df_size):
         for i in range(lm_cnt):
-            x = int(y_coord[j,i*3]/scale)
-            y = int(y_coord[j,i*3+1]/scale)
-            if (x>0 and x <size) and (y>0 and y <size):
-                y_map[j,x,y,i] = 1
-                y_map[j,:,:,i] = gaussian_filter(y_map[j,:,:,i],sigma=1)
-
+            x = int(y_coord[j,i*3]/real_scale)
+            y = int(y_coord[j,i*3+1]/real_scale)
+            # print(x,y)
+            if (x>=0 and x <size) and (y>=0 and y <size):
+                y_map[j,y,x,i] = 20
+                y_map[j,:,:,i] = gaussian_filter(y_map[j,:,:,i],sigma=2)
+                # y_map[j,y,x,i]=1
+    y_map = np.round(y_map,4)
     return y_map
+
+def set_y_center_map(df,scale = 1 , network_scale = 8):
+    columns = df.columns
+    if "height" in columns or "width" in columns:
+        l_m_columns = columns.drop(['image_id' , 'image_category','height','width'])
+    else:
+        l_m_columns = columns.drop(['image_id' , 'image_category'])
+    y_coord = df[l_m_columns].as_matrix()
+    lm_cnt = int(y_coord.shape[1]/3)
+    df_size = y_coord.shape[0]
+    size = int(512/(scale*network_scale))
+    real_scale = 512/size
+    y_map = np.zeros((df_size,size,size,1))
+
+    x_index = np.arange(0,lm_cnt*3 ,3)
+    y_index = np.arange(1,lm_cnt*3 ,3)
+
+
+
+    for j in range(df_size):
+        x_id_array = y_coord[j,x_index]
+        y_id_array = y_coord[j,y_index]
+
+        x_mean = np.mean(x_id_array[x_id_array!=-1])
+        y_mean = np.mean(y_id_array[y_id_array!=-1])
+        x_mean = int(x_mean/real_scale)
+        y_mean = int(y_mean/real_scale)
+
+        y_map[j,y_mean,x_mean,0] = 200
+        y_map[j,:,:,0] = gaussian_filter(y_map[j,:,:,0],sigma=4)
+    y_map = np.round(y_map,4)
+    return y_map
+
 
 def get_x_y(df_size=-1,scale=1,pre_dir="train_pad/",cates=0,flat_x = True):
 
@@ -150,23 +188,6 @@ def get_x_y(df_size=-1,scale=1,pre_dir="train_pad/",cates=0,flat_x = True):
 
 def get_x_y_map(df_size=-1,scale=1,pre_dir="train_pad/",cates=0,flat_x = True):
 
-    path = pre_dir +"Annotations/train_"+categories.get_cate_name(cates)+"_coord_augs.csv"
-    print("Read data from files: ",path)
-    df = pd.read_csv(path)
-    if df_size !=-1:
-        df=df[:df_size]
-    
-    if flat_x:
-        x_train = set_x_flat(df, scale, "train_pad/")
-    else:
-        x_train = set_x_img(df, scale, "train_pad/")
-    y_train = set_y_map(df , scale)
-
-    print("X shape: ",x_train.shape , "Y shape: " , y_train.shape)
-    return x_train,y_train
-
-def get_x_y_map_valid(df_size=-1,scale=1,pre_dir="./train_warm_up_pad/",cates=0,flat_x = True):
-
     path = pre_dir +"Annotations/train_"+categories.get_cate_name(cates)+"_coord.csv"
     print("Read data from files: ",path)
     df = pd.read_csv(path)
@@ -178,9 +199,51 @@ def get_x_y_map_valid(df_size=-1,scale=1,pre_dir="./train_warm_up_pad/",cates=0,
     else:
         x_train = set_x_img(df, scale, "train_pad/")
     y_train = set_y_map(df , scale)
+    y_coord = set_y_coord(df , 1 , True)
+
+    center_map = set_y_center_map(df , scale , 1)
 
     print("X shape: ",x_train.shape , "Y shape: " , y_train.shape)
-    return x_train,y_train    
+    print("Coords shape: ",y_coord.shape , "Map shape: " , center_map.shape)
+    return x_train,y_train,y_coord , center_map
+
+def get_x_y_center_map(df_size=-1,scale=1,pre_dir="train_pad/",cates=0,flat_x = True):
+
+    path = pre_dir +"Annotations/train_"+categories.get_cate_name(cates)+"_coord.csv"
+    print("Read data from files: ",path)
+    df = pd.read_csv(path)
+    if df_size !=-1:
+        df=df[:df_size]
+    
+    if flat_x:
+        x_train = set_x_flat(df, scale, "train_pad/")
+    else:
+        x_train = set_x_img(df, scale, "train_pad/")
+    y_train = set_y_center_map(df , scale)
+
+    print("X shape: ",x_train.shape , "Y shape: " , y_train.shape)
+    return x_train,y_train
+
+
+def get_x_y_map_valid(df_size=-1,scale=1,pre_dir="./train_warm_up_pad/",cates=0,flat_x = True):
+
+    path = pre_dir +"Annotations/train_"+categories.get_cate_name(cates)+"_coord.csv"
+    print("Read data from files: ",path)
+    df = pd.read_csv(path)
+    if df_size !=-1:
+        df=df[:df_size]
+    
+    if flat_x:
+        x_train = set_x_flat(df, scale, pre_dir)
+    else:
+        x_train = set_x_img(df, scale, pre_dir)
+    y_train = set_y_map(df , scale)
+    y_coord = set_y_coord(df , 1 , True)
+    center_map = set_y_center_map(df , scale , 1)
+
+    print("X shape: ",x_train.shape , "Y shape: " , y_train.shape)
+    print("Coords shape: ",y_coord.shape , "Map shape: " , center_map.shape)
+    return x_train,y_train    ,y_coord,center_map
 
 def get_x_y_valid(df_size=-1,scale=1,pre_dir="./train_warm_up_pad/",cates=0,flat_x = True):
 
@@ -273,3 +336,146 @@ if __name__ == "__main__":
     # get_x_y_time(200,scale=1)
     # get_x_y_time(2000,scale=16)
     # get_x_y_time(2000 ,scale=16, flat_x = False)
+
+class data:
+    def __init__(self,X,Y,coords,batch_size,is_train):
+
+        self.X = X
+        self.df_size = X.shape[0]
+        self.batch_size = batch_size
+        self.is_train = is_train
+
+
+        self.start_idx =0
+        self.indices = np.arange(self.df_size)
+        np.random.shuffle(self.indices)
+        if is_train:
+            self.coords = coords
+            self.Y = Y
+
+        print("Init data class...")
+        print("\tX shape: {}\n\tY shape: {}\n\tbatch_size:{}"\
+            .format(self.X.shape,self.Y.shape, self.batch_size))
+
+
+    
+    def get_next_batch(self):
+        batch_size = self.batch_size
+        df_size = self.df_size
+        is_train = self.is_train
+
+        
+        if self.start_idx >= (df_size - batch_size+1):
+            self.start_idx = 0 
+            self.indices = np.arange(self.df_size)
+            np.random.shuffle(self.indices)
+
+        # print(self.start_idx , self.start_idx+batch_size)
+        # print(self.indices)
+        excerpt = self.indices[self.start_idx:self.start_idx + batch_size]
+        x_mini = self.X[excerpt]
+        if is_train:
+            y_mini = self.Y[excerpt] 
+            if self.coords!=None:
+                coords_mini = self.coords[start_idx:start_idx+batch_size]
+            else:
+                coords_mini = None
+        self.start_idx += batch_size
+
+        if is_train:
+            return x_mini , y_mini, coords_mini
+        else:
+            return x_mini
+
+    def get_next_batch_no_random(self):
+        batch_size = self.batch_size
+        df_size = self.df_size
+        is_train = self.is_train
+
+        
+        if self.start_idx >= (df_size - batch_size+1):
+            self.start_idx = 0 
+        x_mini = self.X[start_idx:start_idx+batch_size]
+        if is_train:
+            y_mini = self.Y[start_idx:start_idx+batch_size]
+            if self.coords!=None:
+                coords_mini = self.coords[start_idx:start_idx+batch_size]
+            else:
+                coords_mini = None
+        self.start_idx += batch_size
+
+        if is_train:
+            return x_mini , y_mini, coords_mini
+        else:
+            return x_mini
+        
+class data2:
+    def __init__(self,X,Y,coords,center,batch_size,is_train):
+
+        self.X = X
+        self.df_size = X.shape[0]
+        self.batch_size = batch_size
+        self.is_train = is_train
+
+
+        self.start_idx =0
+        self.indices = np.arange(self.df_size)
+        np.random.shuffle(self.indices)
+        if is_train:
+            self.coords = coords
+            self.Y = Y
+            self.center = center
+
+        print("Init data class...")
+        print("\tX shape: {}\n\tY shape: {}\n\tbatch_size:{}"\
+            .format(self.X.shape,self.Y.shape, self.batch_size))
+
+
+    
+    def get_next_batch(self):
+        batch_size = self.batch_size
+        df_size = self.df_size
+        is_train = self.is_train
+
+        
+        if self.start_idx >= (df_size - batch_size+1):
+            self.start_idx = 0 
+            self.indices = np.arange(self.df_size)
+            np.random.shuffle(self.indices)
+
+        # print(self.start_idx , self.start_idx+batch_size)
+        # print(self.indices)
+        excerpt = self.indices[self.start_idx:self.start_idx + batch_size]
+        x_mini = self.X[excerpt]
+        if is_train:
+            y_mini = self.Y[excerpt] 
+            center_mini = self.center[excerpt]
+            coords_mini = self.coords[excerpt]
+
+        self.start_idx += batch_size
+
+        if is_train:
+            return x_mini , y_mini, coords_mini,center_mini
+        else:
+            return x_mini
+
+    def get_next_batch_no_random(self):
+        batch_size = self.batch_size
+        df_size = self.df_size
+        is_train = self.is_train
+
+        
+        if self.start_idx >= (df_size - batch_size+1):
+            self.start_idx = 0 
+        x_mini = self.X[self.start_idx:self.start_idx+batch_size]
+        if is_train:
+            y_mini = self.Y[self.start_idx:self.start_idx+batch_size]
+            coords_mini = self.coords[self.start_idx:self.start_idx+batch_size]
+            center_mini= self.center[self.start_idx:self.start_idx+batch_size]
+        self.start_idx += batch_size
+
+        if is_train:
+            return x_mini , y_mini, coords_mini,center_mini
+        else:
+            return x_mini
+        
---
+++ train_trousers.py
@@ -0,0 +1,198 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+  
+import tensorflow as tf
+
+slim = tf.contrib.slim
+
+# import vgg
+# import vgg16_coord_model as model
+import numpy as np
+import pandas as pd
+import sklearn.model_selection as sk
+from enum import Enum
+import sys
+import os
+import time
+
+import importlib
+import train_input
+import cpm
+importlib.reload(train_input)
+importlib.reload(cpm)
+
+
+
+def accuracy(result, coord, imsize):
+    df_size = result.shape[0]
+    cnt_size = result.shape[3]
+    
+    scale = (512/(imsize/8))
+    output_result = np.ones((df_size,cnt_size*2))
+    for i in range(df_size):
+        for j in range(cnt_size):
+            heat_map = result[i,:,:,j]
+            map_shape = np.unravel_index(np.argmax(heat_map, axis=None), heat_map.shape)
+            x = map_shape[1]+1
+            y = map_shape[0]+1
+            output_result[i,j*2+0] = x*scale
+            output_result[i,j*2+1] = y*scale
+#             print("x pred: {} origin: {}".format(x*scale,coord[i,j*2]))
+#             print("y pred: {} origin: {}".format(y*scale,coord[i,j*2+1]))
+    #print(output_result)
+    acc_per_row = np.sqrt(np.mean(np.square(output_result-coord) , axis =1))
+    print( acc_per_row)
+    
+    
+
+class Config():
+    
+    def __init__(self,category,imsize , learning_rate):
+        self.category = category
+        self.img_height = imsize
+        self.img_width = imsize
+        self.load_filename = "cpm_"+category+".ckpt"
+        self.save_filename = "cpm_"+category+".ckpt"
+        
+        self.fm_width = self.img_width >> 1
+        self.fm_height = self.img_height >> 1
+        
+        self.learning_rate = learning_rate
+                #file path
+        self.logdir =  os.path.join("./log/train_log/CPM/", category , str(self.learning_rate))
+        self.params_dir = "./params/CPM_with_center/" + category + "/"
+    # =================== modify parameters ==================
+    TAG = "_demo" # used for uniform filename
+               # "_demo": train with demo images
+               # "": (empty) train with ~60000 images
+    batch_size = 20
+    initialize = True # True: train from scratch (should also
+                    # delete the corresponding params files
+                    # in params_dir);
+                     # False: restore from pretrained model
+    steps = "30000"   # if 'initialize = False', set steps to 
+                     # where you want to restore
+    toDistort = False
+    # iterations config
+    max_iteration = 1000
+    checkpoint_iters = 2000
+    summary_iters = 100
+    validate_iters = 2000
+    
+    
+#     category
+    # ========================================================
+
+#     annos_path = "./labels/txt/input/train_annos" + TAG + ".txt"
+#     data_path = "./data/input/train_imgs" + TAG + "/"
+    gpu = '/gpu:0'
+
+    # checkpoint path and filename
+#     logdir = "./log/train_log/"
+#     params_dir = "./params/" + TAG + "/"
+    load_filename = "cpm" + '-' + steps
+
+    
+    # image config
+    points_num = 4
+    fm_channel = points_num + 1
+    #   origin_height = 212
+    #   origin_width = 256
+    
+
+    # feature map config
+
+    sigma = 2.0
+    alpha = 1.0
+    radius = 12
+
+    # random distortion
+    degree = 8
+
+    # solver configinference_person
+    wd = 5e-4
+    stddev = 5e-2
+    use_fp16 = False
+    moving_average_decay = 0.999
+
+
+
+imsize = 512
+total_size = -1
+test_size =30
+category_name = "trousers"
+config = Config(category_name,imsize,0.001)
+
+x_input,y_input,coords_input,maps_input = train_input.get_x_y_map(total_size,512/imsize, cates = category_name, flat_x = False)
+
+input_data = train_input.data2(x_input,y_input,coords_input,maps_input,config.batch_size,is_train=True)
+
+x_test, y_test,coord_test,center_test  = train_input.get_x_y_map_valid(test_size,512/imsize,pre_dir="./train_warm_up_pad/", cates = category_name, flat_x = False)
+test_data = train_input.data2(x_test,y_test,coord_test,center_test,config.batch_size,is_train=True)
+
+###model####
+
+l = 5e-06
+
+print("learning rate: {}".format(l))
+tf.reset_default_graph()
+config = Config(category_name,imsize,l)
+model = cpm.CPM(config)
+predict = model.inference_pose_vgg()
+
+loss = model.loss()
+train_op = model.train_op(loss, model.global_step)
+# accuracy_model = model.accuracy()
+
+
+all_var = tf.trainable_variables()
+
+saver = tf.train.Saver()
+
+
+
+
+init_op = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init_op)
+
+    if not os.path.exists(config.params_dir):
+        os.makedirs(config.params_dir)
+    if os.listdir(config.params_dir) == [] or config.initialize:
+        print ("Initializing Network")
+        sess.run(init_op)
+    else:
+        sess.run(init_op)
+        model.restore(sess, saver, config.load_filename)
+    merged = tf.summary.merge_all()
+    logdir = config.logdir
+
+    writer = tf.summary.FileWriter(logdir, sess.graph)
+    for i in range(5000):
+
+        X_train_mini,y_train_mini,coords_mini,center_mini = input_data.get_next_batch()
+        feed_dict = {
+                    model.images: X_train_mini,
+                    model.labels: y_train_mini,
+                    }
+        sess.run(train_op, feed_dict=feed_dict)
+        if (i + 10) % 10 == 0:
+#             print("accuracy: {}".format(sess.run(accuracy_model, feed_dict=feed_dict)))
+#             print("accuracy: {}".format(len(sess.run(accuracy_model, feed_dict=feed_dict))))
+            print("{} steps Loss: {}".format(i,sess.run(loss, feed_dict=feed_dict)))
+            result=sess.run(predict, feed_dict=feed_dict)
+            accuracy(result,coords_mini,imsize)
+        
+            tmp_global_step = model.global_step.eval()
+            summary = sess.run(merged, feed_dict=feed_dict)    
+            writer.add_summary(summary, tmp_global_step)
+            writer.flush()
+    #abs
+#     feed_dict = {
+#         model.images: x_test,
+#         model.labels: y_test
+#     }
+#     print("Test loss: {}".format(sess.run(predict, feed_dict=feed_dict) ))
+    tmp_global_step = model.global_step.eval()
+    model.save(sess, saver, config.save_filename,tmp_global_step)
